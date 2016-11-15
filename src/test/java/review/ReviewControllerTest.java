1d4c532be82963b4e4c45b12e0db5f243ea1d686
We Can Code IT <you@example.com>
2016-11-15 14:06:46 -0500

    made test file for ReviewController

--- src/test/java/review/ReviewControllerTest.java
+++ src/test/java/review/ReviewControllerTest.java
@@ -1,6 +1,10 @@
 package review;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,6 +23,9 @@ public class ReviewControllerTest {
 	private Review review; 
 	@Mock
 	private Model model;
+	@Mock
+	private Collection<Review> reviews;
+	
 	@Before 
 	public void setup(){
 		MockitoAnnotations.initMocks(this);
@@ -27,5 +34,25 @@ public class ReviewControllerTest {
 	public void shouldAddReviewInTheModel(){
 		long id = 42;
 		when(repository.findById(id)).thenReturn(review);
+		underTest.displayReview(id, model);
+		verify(model).addAttribute("selectedReview", review);//behavior verification
+	}
+	
+	@Test
+	public void shouldReturnSingleReviewView(){
+		int arbitraryId = 42;
+		String result = underTest.displayReview(arbitraryId, model);
+		assertEquals("review-view", result);//string literals need to be in quotation marks (review-view)
+	}
+	@Test
+	public void shouldReturnReviewAll(){
+		String result = underTest.displayAll(model);
+		assertEquals("review-all", result);
+	}
+	@Test
+	public void shouldAddAllReviewsInTheModel(){
+		when(repository.findAll()).thenReturn(reviews);
+		underTest.displayAll(model);
+		verify(model).addAttribute("reviews", reviews);
 	}
 }
